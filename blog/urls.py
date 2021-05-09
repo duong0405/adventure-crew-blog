52d40a9471c2af4eb8717e71df7d17c8c8230d4a
Phuong Duong <phuongx31@gmail.com>
2021-05-09 22:45:40 +0700

    add user profile header page

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("user", views.user_profile),
+    path("user", views.user_profile, name="user-profile"),
     path("/posts", views.posts, name="post-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
 
